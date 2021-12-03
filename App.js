d914c3b10b9bba3976ce5ad329195b41205aca00
BristinaTemneanu <bristina.temneanu@yahoo.com>
2021-12-03 17:42:34 +0200

    Basic counter example with hooks. Read about:
    1. What is state? How can I change it? https://reactnative.dev/docs/state
    2. The document also contain the idea of props. We will talk about them later.

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  //read about react native's hooks. This is used to initialize and change the state of the current component.
+  // When you use the state in the render function, the screen will be rendered again at every change. To update the state,
+  // you must use the setter
+  const [count, setCount] = useState(0);
+
+  let add = () => {
+    setCount(count+1);
+  };
+  let subtract = () => {
+    setCount(count-1);
+  };
+
+  //here is what is rendered
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is your basic counter's value: {count}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => add()}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => subtract()}>
+        <Text>Subtract</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+  //this is the styling.we will move this in another file later
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +39,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#dfac23',
+    borderRadius: 10,
+  }
 });
